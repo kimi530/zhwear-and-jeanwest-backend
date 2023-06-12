58221c542e5125c0b8349c227a660033e7421e34
unknown <mosaferikimia@gmail.comgit comfig ==global user.name>
2023-06-13 01:28:34 +0330

    backend files

---
+++ models/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'BLACK-ZHWEAR',
+    image: '/products/black-600x600.jpg',
+    price: 467000,
+  },
+  {
+    _id: '2',
+    name: 'BLUE-ZHWEAR',
+    image: '/products/blue-600x600.jpg',
+    price: 467000,
+  },
+  {
+    _id: '3',
+    name: 'BLUE-BLANK-TSHIRT',
+    image: '/products/d4-1-600x600.jpg',
+    price: 467000,
+  },
+  {
+    _id: '4',
+    name: 'BLUE-DIDAR-TSHIRT',
+    image: '/products/d4-600x600.jpg',
+    price: 467000,
+  },
+  {
+    _id: '5',
+    name: 'BLACK-TSHIRT',
+    image: '/products/d4-600x600.jpg',
+    price: 467000,
+  },
+  {
+    _id: '6',
+    name: 'BLACK-TSHIRT',
+    image: '/products/d4-600x600.jpg',
+    price: 467000,
+  },
+  {
+    _id: '7',
+    name: 'BLACK-WAFER-SHORTS',
+    image: '/products/DSC70203-Large-600x600.jpeg',
+    price: 548000,
+  },
+  {
+    _id: '8',
+    name: 'BLACK-KHIAL-TSHIRT',
+    image: '/products/k5-600x600.jpg',
+    price: 467000,
+  },
+  {
+    _id: '9',
+    name: 'BLACK-NASER-TSHIRT',
+    image: '/products/k5-600x600.jpg',
+    price: 467000,
+  },
+  {
+    _id: '10',
+    name: 'BLACK-DIDAR-TSHIRT',
+    image: '/products/d5-600x600.jpg',
+    price: 467000,
+  },
+  {
+    _id: '11',
+    name: 'BLACK-DAVAR-TSHIRT',
+    image: '/products/d5-1-600x600.jpg',
+    price: 467000,
+  },
+  {
+    _id: '12',
+    name: 'BLACK-FRIENDSHIP',
+    image: '/products/f5-600x600.jpg',
+    price: 467000,
+  },
+  {
+    _id: '13',
+    name: 'BLACK-PANTS',
+    image: '/products/DSC063361-600x600.jpg',
+    price: 546000,
+  },
+]
+
+module.exports = products
